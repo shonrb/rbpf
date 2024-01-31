53be149a8a50095726423c99a1278079280bc02d
Sean <sa.roberts.b@gmail.com>
2024-01-31 14:39:56 +0000

    Add test

---
+++ test/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let prog = &[
+        0x71, 0x10, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, // ldxh r0, [r1+2]
+        0x95, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00  // exit
+    ];
+
+    // Let's use some data.
+    let mem = &mut [
+        0xaa, 0xbb, 0x11, 0xcc, 0xdd
+    ];
+
+    // This is an eBPF VM for programs reading from a given memory area (it
+    // directly reads from packet data)
+    let vm = rbpf::EbpfVmRaw::new(Some(prog)).unwrap();
+
+    assert_eq!(vm.execute_program(mem).unwrap(), 0x11);
+}
+
